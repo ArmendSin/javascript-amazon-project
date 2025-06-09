166fc8805112b8e58948d1a75ecf235de562081d
As554 <as554@njit.edu>
2025-06-09 16:02:02 -0400

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,8 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days')
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary () {
 
@@ -144,6 +140,7 @@ export function renderOrderSummary () {
 				);
 				container.remove();
 				updateCartQuantity();
+				renderPaymentSummary();
 			});
 		});
 
@@ -191,6 +188,7 @@ export function renderOrderSummary () {
 				quantityLabel.innerHTML = newQuantity;
 
 				updateCartQuantity();
+				renderPaymentSummary();
 			});
 		});
 
@@ -200,6 +198,7 @@ export function renderOrderSummary () {
 					const {productId, deliveryOptionId} = element.dataset;
 					updateDeliveryOption(productId, deliveryOptionId);
 					renderOrderSummary();
+					renderPaymentSummary();
 				})
 			})
 
